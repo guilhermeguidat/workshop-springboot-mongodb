e746c48b4f513674aa6baaed04fcad21530efe6e
Guilherme Guida <gguilhermecontato1@gmail.com>
2024-11-11 10:51:25 -0300

    Inserção de usuário com POST

--- src/main/java/com/guilhermeguida/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermeguida/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.guilhermeguida.workshopmongo.services;
 
 import com.guilhermeguida.workshopmongo.domain.User;
+import com.guilhermeguida.workshopmongo.dto.UserDTO;
 import com.guilhermeguida.workshopmongo.repository.UserRepository;
 import com.guilhermeguida.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
